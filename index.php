39909eb92b1302328d8590c3cdca0fe0fa148b2a
Muhammad Rana <muhammadrana523@gmail.com>
2021-08-22 11:29:20 +0500

    Changes

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
     </head>
     <body>
         <div class="container">
-            <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            <h4 class="sent-notification"></h4>
+            <form id="myForm">
                 <div class="row">
                     <div class="form-group col-6">
                         <label for="from">From</label>
@@ -25,13 +26,37 @@
                         <textarea name="message" class="form-control" id="message" rows="5"></textarea>
                     </div>
                 </div>
-                <input type="submit" name="submit" class="btn btn-primary" value="Send">
+                <button type="button" onclick="sendEmail()" class="btn btn-primary" value="Send an Email">Send</button>
             </form>
         </div>
 
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+        function sendEmail(){
+            var from = $("#from");
+            var to = $("#to");
+            var message = $("#message");
+
+            $.ajax({
+                url: 'sendEmail.php',
+                method: 'POST',
+                dataType: 'json',
+                data:{
+                    from: from.val(),
+                    to: to.val(),
+                    message: message.val(),
+                },
+                success: function (response){
+                    $('#myForm')[0].reset();
+                    $('.sent-notification').text("Message Sent Successfully");
+                }
+            });
+        }
+    </script>
     </body>
 </html>
 <?php
---
+++ sendEmail.php
@@ -0,0 +1,41 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+
+    if (isset($_POST['from']) && isset($_POST['to'])){
+        $from = $_POST['from'];
+        $to = $_POST['to'];
+        $message = $_POST['message'];
+
+        require_once "PHPMailer/PHPMailer.php";
+        require_once "PHPMailer/SMTP.php";
+        require_once "PHPMailer.Exception.php";
+
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->Host = "smtp.gmail.com";
+        $mail->SMTPAuth = true;
+        $mail->Username = "bfoot171@gmail.com";
+        $mail->Password = "greenmac20";
+        $mail->Port = 465;
+        $mail->SMTPSecure = "ssl";
+
+        $mail->isHTML(true);
+        $mail->setFrom($to);
+        $mail->addAddress("bfoot171@gmail.com");
+        $mail->Subject = ("$to (Hello)");
+        $mail->Body = $message;
+
+        if ($mail->send()){
+            $status = "Success";
+            $response = "Email is sent";
+        }
+        else{
+            $status = "failed";
+            $response = "Email is not send". $mail->ErrorInfo;
+        }
+
+        exit(json_encode(array("status" => $status, "response" => $response)));
+
+    }
+
+?>
